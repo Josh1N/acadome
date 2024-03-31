b3ff6130f9396538c67a47fb0c9fbfb351c91da7
Joshua Ngwarai <joshuangwarai@Joshuas-MacBook-Pro.local>
2024-03-31 23:53:19 +0200

    Added Rooms table

--- base/models.py
+++ base/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Room(models.Model):
+    # host = models.CharField(max_length=255)
+    # topic = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    # participants = models.ManyToManyField(Participant)    
+    updated_at = models.DateTimeField(auto_now = True)
+    created_at = models.DateTimeField(auto_now_add = True)
+    
+    def __str__(self):
+        return self.name()
